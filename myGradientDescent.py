03acd60139424e090804d26783cb67e47b8e9510
jokandre <jorgeandre93@yahoo.com.br>
2017-01-20 17:39:35 +0800

    bugging

--- myGradientDescent.py
+++ myGradientDescent.py
@@ -5,8 +5,8 @@ def compute_error_for_line_given_points(m, b, points):
 	totalError = 0
 	#for every point in dataset
 	for i in range(0, len(points)):
-		x = points[i,0] #get x value
-		y = points[i,1] #get y value
+		x = points[i, 0] #get x value
+		y = points[i, 1] #get y value
 		#calc distance between point and line
 		totalError += (y - (m*x + b))**2
 	return totalError / float(len(points))
