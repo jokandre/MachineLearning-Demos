a90115fca795bf56ca20aecb69d6a989883d2589
jokandre <jokandre93@yahoo.com.br>
2017-01-21 22:52:08 +0800

    plot after both algo

--- myDescent.py
+++ myDescent.py
@@ -7,37 +7,35 @@ def h_theta(t0, t1, x):
 
 # total error, j(theta)
 def cost_SSE( points, t0, t1 ):
-    #Sum Squared Error funtion
+    # Sum Squared Error funtion
     #J = sum([(t0 + t1* points[i, 0] - points[i, 1])**2 for i in range(0 , len(points))])
 
-    totalError = 0
+    totalError = 0 # same as J above
+
     # Sum Squared distance between line and all data points
     for i in range(0, len(points)):
-        x_i = points[i, 0] #get x value from dataset
-        y_i = points[i, 1] #get y value from dataset
+        x_i = points[i][0] #get x value from dataset
+        y_i = points[i][1] #get y value from dataset
         totalError += ( h_theta(t0, t1, x_i)  - y_i)**2
-
-    return totalError / 2*float(len(points))
-
+    return totalError# / 2*float(len(points))
 
 
-def gradient_descent(alpha, x, y, ep=0.0001, max_iter=10000):
+def gradient_descent( points, t0, t1, alpha = 0.01, max_iter=10000, ep = 0.01):
     converged = False
     iter = 0
-    m = x.shape[0] # number of samples
 
-    # initial theta
-    t0 = np.random.random(x.shape[1])
-    t1 = 0
+    total_samples = len(points) # total samples in dataseet
+    x= points[ : , 0] # get all x values from dataset
+    y= points[ : , 1] # get all y values from dataset
 
-    # total error, J(theta)
-    J = sum([(t0 + t1*x[i] - y[i])**2 for i in range(m)])
+
+    J = sum([(t0 + t1* x[i] - y[i])**2 for i in range(0 , len(points))])
 
     # Iterate Loop
     while not converged:
         # for each training sample, compute the gradient (d/d_theta j(theta))
-        grad0 = 1.0/m * sum([(t0 + t1*x[i] - y[i]) for i in range(m)])
-        grad1 = 1.0/m * sum([(t0 + t1*x[i] - y[i])*x[i] for i in range(m)])
+        grad0 = 1.0/total_samples * sum([(t0 + t1* x[i] - y[i]) for i in range(total_samples)])
+        grad1 = 1.0/total_samples * sum([(t0 + t1* x[i] - y[i]) * x[i] for i in range(total_samples)])
 
         # update the theta_temp
         temp0 = t0 - alpha * grad0
@@ -48,9 +46,9 @@ def gradient_descent(alpha, x, y, ep=0.0001, max_iter=10000):
         t1 = temp1
 
         # mean squared error
-        e = sum( [ (t0 + t1*x[i] - y[i])**2 for i in range(m)] )
+        e = sum( [ (t0 + t1*x[i] - y[i])**2 for i in range(total_samples)] )
 
-        if abs(J-e) <= ep:
+        if abs(J - e) <= ep:
             print ('Converged, iterations: ', iter, '!!!')
             converged = True
 
@@ -61,4 +59,4 @@ def gradient_descent(alpha, x, y, ep=0.0001, max_iter=10000):
             print ('Max interactions exceeded!')
             converged = True
 
-    return t0,t1
+    return t0,t1, iter
